ee24b584c1442cb0a4d5c4fc898ada4b3c294a3e
RAHUL M <rahulm6124@gmail.com>
2025-06-18 23:35:47 +0530

    Code Added..

---
+++ GenerateStrongPassword.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+#include<ctime>
+#include<cstdlib>
+
+using namespace std;
+
+string getPassword(int length) {
+    string password="";
+    string characters = "enillabrosumneEnotrymadthaediniasteNRMREHIAIEBfRuDkCUlgDYWIVD125268ra416791gj7t2j90jy179dugene243014rrauillwbate517";
+    int charsize = characters.size();
+    srand(time(0));
+    int randomIndex;
+    for(int i=0;i<length;i++){
+        randomIndex = rand() % charsize;
+        password = password + characters[randomIndex];
+    }
+    return password;
+}
+
+
+int main(){
+    int length;
+    cout<<"Enter the length of the Password :"<<endl;
+    cin>>length;
+    string password = getPassword(length);
+    cout<<"Generated Password : "<<password<<endl;
+
+    return 0;
+}
